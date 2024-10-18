cb05d1b9e1df182da4890a19dd934a287a712d6b
a1romero <akromero@wm.edu>
2024-10-18 13:31:28 -0400

    example added

--- src/v2.py
+++ src/v2.py
@@ -2,7 +2,7 @@ import itertools
 import pandas as pd
 import numpy as np
 import os
-import processing
+import src.processing as processing
 
 def shuffle_deck(seed:None):
     '''Generates a single shuffled deck'''
@@ -20,21 +20,7 @@ def play_n_games(n, data):
     results = {}
     for folder in filename:
         if folder == 'cards_win/' or folder == 'tricks_win/':
-            results[folder] = sum_games(f'{data}{folder}', True)
+            results[folder] = processing.sum_games(f'{data}{folder}', True)
         elif folder == 'cards_draw/' or folder == 'tricks_draw/':
-            results[folder] = sum_games(f'{data}{folder}', False)
+            results[folder] = processing.sum_games(f'{data}{folder}', False)
     return results
-
-def sum_games(data = 'data/'):
-    '''Take all of the arrays in the /data folder, and add them together/divide by number of files to get the average'''
-    files = [file for file in os.listdir(data) if os.path.isfile(os.path.join(data, file))] # iterate through /data directory, only process files
-    games_total = None # where the sum of the games is going
-    for file in files:
-        file_path = os.path.join(data,file) # get file name and directory
-        game = np.load(file_path, allow_pickle=True) # load the file
-        if games_total is None:
-            games_total = game # initialize games_total sum array
-        else:
-            games_total += game
-    num_games = len(files)
-    return np.divide(games_total, num_games) # divide each individual element by the number of games played
